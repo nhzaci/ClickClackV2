d3c73d73cf5f6c5f513ee61dc9f067e999143264
Zack <warrior.killz@gmail.com>
2020-06-16 16:56:17 +0800

    Imported vue meta

--- src/main.js
+++ src/main.js
@@ -2,14 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify';
+import vuetify from './plugins/vuetify'
+import VueMeta from 'vue-meta'
 
 //Plugins
 import VueCookies from 'vue-cookies'
 
 Vue.config.productionTip = false
+
 Vue.use(VueCookies)
 Vue.$cookies.config('7d', '', '', '', 'Strict')
+
+Vue.use(VueMeta)
 
 new Vue({
   router,
